f7347008682fff7eee3af6ed305d8f77d379b390
nicolaswiedel <nicolaswiedel@googlemail.com>
2024-05-07 18:02:29 +0200

    Chapter 3.4. Scriptable Object

---
+++ Dragoncraft/Assets/Scripts/Configuration/LevelConfiguration.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dragoncraft
+{
+    [CreateAssetMenu(menuName = "Dragoncraft/New Configuration")]
+    public class LevelConfiguration : ScriptableObject
+    {
+        public List<LevelItem> levelItems = new List<LevelItem>();
+        
+        public LevelItem FindByType(LevelItemType type)
+        {
+            return levelItems.Find(item => item.Type == type);
+        }
+    }
+}
+
